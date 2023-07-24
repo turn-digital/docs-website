f40319afea429af7be25b3175ecdacec937da453
Vladyslav Tkachenko <vlad@wtf.lv>
2023-07-24 21:25:52 +0300

    changes in config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,24 @@
+/** @type {import('next').NextConfig} */
 const withNextra = require("nextra")({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.tsx",
 });
 
-module.exports = withNextra({
-  basePath: "/turn-digital.github.io/docs-website",
+const isProduction = process.env.NODE_ENV === "production";
+const assetPrefix = isProduction ? "/docs-website" : "";
+
+const nextConfig = {
   images: {
     unoptimized: true,
   },
-});
+  reactStrictMode: true,
+  swcMinify: true,
+  trailingSlash: true,
+  assetPrefix,
+  basePath: assetPrefix,
+};
+
+module.exports = {
+  ...withNextra(),
+  ...nextConfig,
+};
